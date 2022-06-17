7310fa287a9e0d7d38c6595047fe1c1cf39112fc
KoMaR005 <103891698+KoMaR005@users.noreply.github.com>
2022-06-17 20:18:56 +0600

    VeNuM

--- plugins/nv-es putoo_mp3.js
+++ plugins/nv-es putoo_mp3.js
@@ -9,7 +9,7 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /извини|eeesss putoo|es putoo|esputoo/i
+handler.customPrefix = /Извини|eeesss putoo|es putoo|esputoo/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100

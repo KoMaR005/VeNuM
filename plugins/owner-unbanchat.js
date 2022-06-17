dea1cc5fcafd5e6383b4699572fe8df258b864f9
KoMaR005 <103891698+KoMaR005@users.noreply.github.com>
2022-06-17 19:00:17 +0600

    VeNuM

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -4,6 +4,6 @@ m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄�
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^разбанчат$/i
+handler.command = /^разблокчат$/i
 handler.rowner = true
 export default handler
